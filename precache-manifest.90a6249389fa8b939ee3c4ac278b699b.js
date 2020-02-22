8e79c8f6c3279c5aed01b11c5ec6159fd1e987a6
Willy David Da Conceicao Lozada <willydavidlozada@gmail.com>
2020-02-21 19:01:45 -0500

    Updates

---
+++ precache-manifest.90a6249389fa8b939ee3c4ac278b699b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a5c2a514ff2065b3fa2297e2871d6f",
+    "url": "/deploy-app-video/index.html"
+  },
+  {
+    "revision": "7b225b963907c084b71e",
+    "url": "/deploy-app-video/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/deploy-app-video/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/deploy-app-video/static/js/2.b6fabe4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b225b963907c084b71e",
+    "url": "/deploy-app-video/static/js/main.d6ef5cec.chunk.js"
+  },
+  {
+    "revision": "56b168296ca9f07b704c",
+    "url": "/deploy-app-video/static/js/runtime-main.c0388872.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-app-video/static/media/logo.5d5d9eef.svg"
+  }
+]);
